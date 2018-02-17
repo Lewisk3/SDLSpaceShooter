78ffa4ce2e8171cd873f6f1a61831b7523835e80
Lewisk3 <korbin3lewis@gmail.com>
2018-02-17 11:16:29 -0800

    Update game_objects.c

--- game_objects.c
+++ game_objects.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "structdata.h"
+#include "sprite_manager.h"
 
 Object* createObject(int type, int x, int y, int w, int h, int extbytes)
 {
@@ -13,6 +14,11 @@ Object* createObject(int type, int x, int y, int w, int h, int extbytes)
     Object* enemy;
     enemy = malloc(extbytes);
     enemy->sprite_index = 0;
+    enemy->angle = 0;
+    enemy->xscale = 1;
+    enemy->yscale = 1;
+    enemy->w = w;
+    enemy->h = h;
     enemy->x = x;
     enemy->y = y;
     enemy->w = w;
@@ -20,4 +26,15 @@ Object* createObject(int type, int x, int y, int w, int h, int extbytes)
     enemy->type = type;
 
     return enemy;
+}
+
+void objectImageTransform(Image* img, Object* obj)
+{
+    img->unit.x = obj->x + (img->spritew/2);
+    img->unit.y = obj->y + (img->spriteh/2);
+}
+
+void drawObject(SDL_Renderer* r, Image* img, Object* obj)
+{
+    drawSpriteEx(r, img, obj->x, obj->y, obj->sprite_index, obj->angle, obj->xscale, obj->yscale, 0);
 }
