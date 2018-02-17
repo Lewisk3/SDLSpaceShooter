d0eeeeab85a7bb68b56447b958ce91252096b1a2
Lewisk3 <korbin3lewis@gmail.com>
2018-02-17 14:47:14 -0800

    Update game_objects.h

--- game_objects.h
+++ game_objects.h
@@ -1,7 +1,8 @@
 #ifndef GAME_OBJECTS_H
 #define GAME_OBJECTS_H
 
-Object* createObject(int, int, int, int, int, int);
+Object* createObject(int, int, int, int, int, bool, int);
 void objectImageTransform(Image*, Object* );
+Object* checkObjectCollide(ObjectList*, Object*);
 
 #endif // GAME_OBJECTS_H
