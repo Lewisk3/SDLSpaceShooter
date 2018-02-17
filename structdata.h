8f8ef3269ce77de02294971ca1759f17506174ec
Lewisk3 <korbin3lewis@gmail.com>
2018-02-17 11:17:39 -0800

    Update structdata.h

--- structdata.h
+++ structdata.h
@@ -6,6 +6,8 @@
 typedef enum
 {
     OBJ_BASICENEMY,
+    OBJ_SMALLBULLET,
+    OBJ_DEAD,
 } ObjectTypes;
 
 typedef struct
@@ -15,6 +17,7 @@ typedef struct
     int LEFT;
     int RIGHT;
     int SHOOT;
+    int ALTSHOOT;
 
 } InputButtons;
 
@@ -23,6 +26,8 @@ typedef struct
    SDL_Surface* surface;
    SDL_Texture* texture;
    SDL_Rect unit;
+   int spritew;
+   int spriteh;
    char* name;
 } Image;
 
@@ -33,7 +38,9 @@ typedef struct
     int height;
     float xpos;
     float ypos;
+    double angle;
     float speed;
+    int firerate;
     float friction;
     float xvel;
     float yvel;
@@ -52,12 +59,33 @@ typedef struct
 {
     int type;
     int sprite_index;
+    double angle;
+    double xscale;
+    double yscale;
     int x;
     int y;
     int w;
     int h;
 
 } Object;
+
+typedef struct
+{
+    int type;
+    int sprite_index;
+    double angle;
+    double xscale;
+    double yscale;
+    int x;
+    int y;
+    int w;
+    int h;
+    int dmg;
+    int speed;
+    int xvel;
+    int yvel;
+
+} Bullet;
 
 typedef struct node_object
 {
