8484864bdfaa5e4ea291ad667463e1fdd6b823c9
Lewisk3 <korbin3lewis@gmail.com>
2018-02-17 14:46:17 -0800

    Update main.c

--- main.c
+++ main.c
@@ -52,6 +52,8 @@ typedef enum {
     static Image* enemyships;
     static Image* basicback;
     static Image* lasers;
+    static Image* impacts;
+    static Image* explosion;
 
 // Declarations
 
@@ -61,6 +63,7 @@ void freeImage(Image*);
 void drawImage(SDL_Renderer*, Image*);
 void WindowHandler(SDL_Event* );
 void ButtonHandler(InputButtons*, SDL_Event* , uint8_t* );
+bool checkTicks(uint32_t timer);
 SDL_Keycode getKey(SDL_Event* );
 void Update();
 void UpdateObjects();
@@ -132,6 +135,14 @@ int main(int argc, char** argv)
     freeImage(plr->sprite);
     free(title);
     free(plr);
+    free(plr->timers);
+    free(playerships);
+    free(enemyships);
+    free(basicback);
+    free(lasers);
+    free(impacts);
+    free(gameobjects);
+    free(explosion);
     SDL_DestroyWindow(win);
     SDL_Quit();
     return 0;
@@ -149,20 +160,23 @@ void Initalize()
     boundkeys.BACK     = SDLK_s;
     boundkeys.LEFT     = SDLK_a;
     boundkeys.RIGHT    = SDLK_d;
-    boundkeys.SHOOT    = SDL_BUTTON_LEFT;
-    boundkeys.ALTSHOOT = SDL_BUTTON_RIGHT;
+    boundkeys.SHOOT    = SDLK_SPACE;
+    boundkeys.ALTSHOOT = SDL_BUTTON_LEFT;
     playerships = loadSprites(renderer, "Resources/ships.png",0,0,48,45);
     enemyships  = loadSprites(renderer, "Resources/enemyships.png",0,0,48,45);
     basicback   = loadImage(renderer, "Resources/basicback.jpg", 0, 0);
-    lasers      = loadSprites(renderer, "Resources/lasers.png",0,0,16,22);
+    lasers      = loadSprites(renderer, "Resources/lasers.png",0,0,18,22);
+    impacts     = loadSprites(renderer, "Resources/impacts.png",0,0,14,14);
+    explosion   = loadSprites(renderer, "Resources/explosion.png",0,0,64,64);
 
     gameobjects = createObjectList();
 
     int i;
-    for(i = 0; i < 5; i++)
+    for(i = 0; i < 20; i++)
     {
-        Object* obj = createObject(OBJ_BASICENEMY,random_range(1, WINDOW_WIDTH-48),random_range(-550, 0),20,40,00);
+        Enemy* obj = createObject(OBJ_BASICENEMY,random_range(1, WINDOW_WIDTH-48),random_range(-550, 0),20,40,true,sizeof(Enemy));
         obj->sprite_index = 2;
+        obj->hp = 20;
         addObject(gameobjects,obj);
     }
 
@@ -177,9 +191,11 @@ void Initalize()
     levelbg->img->unit.h = WINDOW_HEIGHT;
 
     plr = malloc(sizeof(Player));
+    plr->timers = calloc(10, sizeof(uint32_t));
     plr->sprite = playerships;
     plr->width  = 20;
     plr->height = 40;
+    plr->firerate = 100;
     plr->speed  = 6;
     plr->image_index = 2;
     plr->friction = 3.8;
@@ -201,10 +217,17 @@ void Update()
         if(active_buttons & BT_RIGHT) plr->xvel += plr->speed/plr->friction;
         if(active_buttons & BT_FIRE)
         {
-            Bullet* newbullet = createObject(OBJ_SMALLBULLET, plr->xpos, plr->ypos - 18,8,22,sizeof(Bullet));
-            newbullet->angle = random_range(-30,30);
-            newbullet->speed = 10;
-            addObject(gameobjects, newbullet);
+            if( (SDL_GetTicks() - plr->timers[0]) >= plr->firerate )
+            {
+                register int i;
+                Bullet* newbullet = createObject(OBJ_SMALLBULLET, plr->xpos, plr->ypos - 18,8,22,true,sizeof(Bullet));
+                newbullet->angle = random_range(-5,5);
+                newbullet->speed = 15;
+                newbullet->dmg = 3;
+                addObject(gameobjects, newbullet);
+                plr->timers[0] = SDL_GetTicks();
+            }
+
         }
                 //Move player.
         if(plr->xvel >  plr->speed) plr->xvel = plr->speed;
@@ -248,14 +271,19 @@ void UpdateObjects()
         }
         if( current->obj->type == OBJ_BASICENEMY)
         {
-            current->obj->xscale += 0.01;
-            current->obj->yscale += 0.01;
-            if(current->obj->y > WINDOW_HEIGHT+current->obj->h)
+            Enemy* oenemy = current->obj;
+            if(oenemy->pos.y > WINDOW_HEIGHT+oenemy->pos.h)
             {
-               current->obj->y = random_range(-500,-50);
-               current->obj->x = random_range(0, WINDOW_WIDTH-current->obj->w);
+               oenemy->pos.y = random_range(-500,-50);
+               oenemy->pos.x = random_range(0, WINDOW_WIDTH-oenemy->pos.w);
             }
-            current->obj->y+=2;
+            oenemy->pos.y+=2;
+
+            if(oenemy->hp <= 0)
+            {
+                addObject(gameobjects, createObject(OBJ_EXPLOSION, oenemy->pos.x, oenemy->pos.y, 5,5,false, 0));
+                oenemy->type = OBJ_DEAD;
+            }
         }
         else if( current->obj->type == OBJ_SMALLBULLET)
         {
@@ -263,9 +291,30 @@ void UpdateObjects()
             obullet->xvel = cos( (obullet->angle-90) * PI / 180)  * obullet->speed;
             obullet->yvel = sin( (obullet->angle-90) * PI / 180 ) * obullet->speed;
 
-            obullet->x += obullet->xvel;
-            obullet->y += obullet->yvel;
-            if(obullet->y < 0) obullet->type = OBJ_DEAD;
+            obullet->pos.x += obullet->xvel;
+            obullet->pos.y += obullet->yvel;
+            Enemy* target = checkObjectCollide(gameobjects, obullet);
+            if(target != NULL && target->type != OBJ_SMALLBULLET)
+            {
+                target->hp-=obullet->dmg;
+                obullet->type = OBJ_DEAD;
+                Object* hit = createObject(IMG_TIMED, obullet->pos.x, obullet->pos.y, 5,5,false,0);
+                hit->deltatime = SDL_GetTicks();
+                hit->sprite_index = current->obj->sprite_index;
+                addObject(gameobjects, hit);
+            }
+            if(obullet->pos.y < 0) obullet->type = OBJ_DEAD;
+        }
+        else if( current->obj->type == IMG_TIMED)
+        {
+            if( (SDL_GetTicks() - current->obj->deltatime) >= 120 ) current->obj->type = OBJ_DEAD;
+        }
+        else if( current->obj->type == OBJ_EXPLOSION)
+        {
+           if(current->obj->sprite_index >= 15)
+           {
+              current->obj->type = OBJ_DEAD;
+           }
         }
         objID ++;
         current = current->next;
@@ -278,7 +327,6 @@ void Draw()
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, levelbg->img->texture, &levelbg->cam, &levelbg->img->unit);
         DrawObjects();
-        drawSprite(renderer, playerships, 10,10, 2);
         SDL_RenderPresent(renderer);
 }
 void DrawObjects()
@@ -297,8 +345,13 @@ void DrawObjects()
             break;
             case OBJ_SMALLBULLET:
                 drawObject(renderer, lasers,current->obj);
+                //animateSprite(renderer, lasers, 80, current->obj->pos.x, current->obj->pos.y, &current->obj->sprite_index, &current->obj->deltatime);
             break;
-            default:
+            case IMG_TIMED:
+                drawObject(renderer, impacts, current->obj);
+            break;
+            case OBJ_EXPLOSION:
+                animateSprite(renderer, explosion, 80, current->obj->pos.x, current->obj->pos.y, &current->obj->deltatime, &current->obj->sprite_index);
             break;
         }
         current = current->next;
@@ -327,10 +380,12 @@ void ButtonHandler(InputButtons* input, SDL_Event* ev, uint8_t* abuttons)
     if(ev->type == SDL_KEYDOWN)
     {
         int key = ev->key.keysym.sym;
-        if(key == input->FORWARD)  *abuttons |= BT_FORWARD;
-        if(key == input->BACK)     *abuttons |= BT_BACK;
-        if(key == input->LEFT)     *abuttons |= BT_LEFT;
-        if(key == input->RIGHT)    *abuttons |= BT_RIGHT;
+        if(key == input->FORWARD)   *abuttons |= BT_FORWARD;
+        if(key == input->BACK)      *abuttons |= BT_BACK;
+        if(key == input->LEFT)      *abuttons |= BT_LEFT;
+        if(key == input->RIGHT)     *abuttons |= BT_RIGHT;
+        if(key == input->SHOOT)     *abuttons |= BT_FIRE;
+        if(key == input->ALTSHOOT)  *abuttons |= BT_ALTFIRE;
     }
     else if(ev->type == SDL_KEYUP)
     {
@@ -339,6 +394,8 @@ void ButtonHandler(InputButtons* input, SDL_Event* ev, uint8_t* abuttons)
         if(key == input->BACK)     *abuttons &= ~BT_BACK;
         if(key == input->LEFT)     *abuttons &= ~BT_LEFT;
         if(key == input->RIGHT)    *abuttons &= ~BT_RIGHT;
+        if(key == input->SHOOT)    *abuttons &= ~BT_FIRE;
+        if(key == input->ALTSHOOT) *abuttons &= ~BT_ALTFIRE;
     }
     else if(ev->type == SDL_MOUSEBUTTONDOWN)
     {
