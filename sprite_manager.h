d6adf25e286919bf6f28cf998b86b5d5f5fac6a8
Lewisk3 <korbin3lewis@gmail.com>
2018-02-19 03:20:04 -0800

    Update sprite_manager.h

--- sprite_manager.h
+++ sprite_manager.h
@@ -2,8 +2,8 @@
 #define SPRITE_MANAGER_H
 
 void drawSprite(SDL_Renderer*, Image*, int, int, int);
-void drawSpriteEx(SDL_Renderer*, Image*, int, int, int, double, double, double, SDL_RendererFlip);
-void animateSprite(SDL_Renderer* , Image* , uint32_t , int , int , uint32_t* , int* );
+void drawSpriteEx(SDL_Renderer*, Image*, int, int, int, double, SDL_RendererFlip);
+void animateSprite(SDL_Renderer* , Image* , uint32_t , int , int , uint32_t* , int* , bool);
 
 
 #endif // SPRITE_MANAGER_H
