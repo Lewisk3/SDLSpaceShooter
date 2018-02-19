d960db60bbcd5e2e5375bd5542e9ef2ae0badea5
Lewisk3 <korbin3lewis@gmail.com>
2018-02-19 03:19:21 -0800

    Update game_objects.h

--- game_objects.h
+++ game_objects.h
@@ -1,8 +1,11 @@
 #ifndef GAME_OBJECTS_H
 #define GAME_OBJECTS_H
 
-Object* createObject(int, int, int, int, int, bool, int);
-void objectImageTransform(Image*, Object* );
-Object* checkObjectCollide(ObjectList*, Object*);
+Object* createObject(ObjectLogic, Image*, int, int, int, int, bool);
+ObjectLogic* createObjectThinker(ObjectTypes, void* , int);
+void tickObject(Object* );
+void objectImageTransform(Image* ,Object* );
+Object* checkObjectCollide(ObjectList* ,Object* );
+void drawObject(SDL_Renderer* ,Object* );
 
 #endif // GAME_OBJECTS_H
