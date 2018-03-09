edb56ffa015c6d342df1d249a1dae742ef8f6c9c
Lewisk3 <korbin3lewis@gmail.com>
2018-03-08 22:53:09 -0800

    Update sprite_manager.c

--- sprite_manager.c
+++ sprite_manager.c
@@ -15,8 +15,8 @@ void drawSprite(SDL_Renderer* r, Image* img, int x, int y, int ind)
     ind = (row > 0) ? ind-(maxind*row): ind;
     SDL_Rect spr = {ind*width, row*height, width, height};
     SDL_Rect spr_unit = img->unit;
-    spr_unit.x = x - (width/2);
-    spr_unit.y = y - (height/2);
+    spr_unit.x = x - ((width*img->xscale) /2);
+    spr_unit.y = y - ((height*img->yscale) /2);
     spr_unit.w = width * img->xscale;
     spr_unit.h = height * img->yscale;
     SDL_SetTextureColorMod(img->texture, img->color->r, img->color->g, img->color->b);
@@ -27,6 +27,7 @@ void drawSprite(SDL_Renderer* r, Image* img, int x, int y, int ind)
 
 void animateSprite(SDL_Renderer* r, Image* img, uint32_t spd, int x, int y, uint32_t* timer, int* ind, bool draw)
 {
+   // puts("Begin animateSprite");
     int anim = *ind;
     if(draw)drawSprite(r, img, x, y, anim);
     if( (SDL_GetTicks() - *timer) >= spd )
@@ -35,8 +36,10 @@ void animateSprite(SDL_Renderer* r, Image* img, uint32_t spd, int x, int y, uint
         int maxind = (floor((img->surface->w) / img->spritew)-1);
         int maxrows = img->surface->h/img->spriteh;
         if(*ind > (maxind+1)*maxrows) *ind = 0;
+        //printf("Frame: %i/%i \n",*ind,maxind);
         *timer = SDL_GetTicks();
     }
+    //puts("End animateSprite");
 }
 
 void drawSpriteEx(SDL_Renderer* r, Image* img, int x, int y, int ind, double angle, SDL_RendererFlip flip)
@@ -44,14 +47,14 @@ void drawSpriteEx(SDL_Renderer* r, Image* img, int x, int y, int ind, double ang
     int width = img->spritew;
     int height = img->spriteh;
     int maxind = floor((img->surface->w) / width)-1;
-    int row = floor( ind/(maxind+1) );
+    int row = floor( ind/(maxind) );
     ind = (row > 0) ? ind-(maxind*row): ind;
-    SDL_Rect spr = {ind*width, row*height, ind+width, height};
+    SDL_Rect spr = {ind*width, row*height, width, height};
     SDL_Rect unit_ex = img->unit;
     unit_ex.w = width*img->xscale;
     unit_ex.h = height*img->yscale;
-    unit_ex.x = x - (width/2);
-    unit_ex.y = y - (height/2);
+    unit_ex.x = x - ((width*img->xscale) /2);
+    unit_ex.y = y - ((height*img->yscale) /2);
     SDL_SetTextureColorMod(img->texture, img->color->r, img->color->g, img->color->b);
     SDL_SetTextureAlphaMod(img->texture, img->alpha);
     SDL_SetTextureBlendMode(img->texture, img->blend);
